05da22cf7e45511c85a3d2b6ac39204146882dbd
fuscoyu <fuscoyu@gmail.com>
2024-05-06 17:55:48 +0800

    [treesitter] support vimdoc

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -6,7 +6,7 @@ local M = {
 
 function M.config()
   require("nvim-treesitter.configs").setup {
-    ensure_installed = { "lua", "markdown", "markdown_inline", "bash", "python", "go" },
+    ensure_installed = { "lua", "markdown", "markdown_inline", "bash", "python", "go", "vimdoc" },
     highlight = { enable = true },
     indent = { enable = true },
     sync_install = false,
